65c5bf430500674e434ee6ae008e012fc20bcb84
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2024-02-02 14:27:10 +0800

    push stuff
    - stuff works atm

--- crates/sim/src/main.rs
+++ crates/sim/src/main.rs
@@ -1,11 +1,63 @@
-
 #![allow(unused)]
 
 mod traits;
+use futures::future::{self, Ready};
+use tarpc::{
+    client, context,
+    server::{self, Channel},
+};
 
-fn main() {
-    println!("Hello, world!");
+#[tarpc::service]
+trait SomeRemoteMethods {
+    async fn whats_up(name: String) -> String;
+    async fn whats_there(which: bool) -> String;
 }
 
+#[derive(Clone)]
+struct FunServer;
+
+impl SomeRemoteMethods for FunServer {
+    async fn whats_up(self, context: ::tarpc::context::Context, name: String) -> String {
+        "asdasd".to_string()
+    }
+
+    async fn whats_there(self, context: ::tarpc::context::Context, which: bool) -> String {
+        "Asdasddasdas".to_string()
+    }
+}
+
+#[tarpc::service]
+trait World {
+    /// Returns a greeting for name.
+    async fn hello(name: String) -> String;
+}
+
+// This is the type that implements the generated World trait. It is the business logic
+// and is used to start the server.
+#[derive(Clone)]
+struct HelloServer;
+
+impl World for HelloServer {
+    async fn hello(self, _: context::Context, name: String) -> String {
+        format!("Hello, {name}!")
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+
+    let (client_transport, server_transport) = tarpc::transport::channel::unbounded();
+    let server = tarpc::server::BaseChannel::with_defaults(server_transport);
+
+    tokio::spawn(async { server.execute(FunServer.serve()) });
+
+    // let client = SomeRemoteMethodsClient::new(Default::default(), client_transport).spawn();
+    let client = SomeRemoteMethodsClient::new(client::Config::default(), client_transport).spawn();
+
+    let x = client.whats_up(context::current(), false.to_string()).await;
 
 
+
+
+}
--- crates/sim/src/traits.rs
+++ crates/sim/src/traits.rs
@@ -1,6 +1,8 @@
 //! Common traits and data structures
 //!
 
+use std::path::Iter;
+
 /// Status of simulation.
 #[derive(Clone, Copy, Debug)]
 pub enum SimStatus {
@@ -10,20 +12,108 @@ pub enum SimStatus {
     Stop,
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Event;
+
+#[derive(Clone, Copy, Debug)]
+struct Process;
+
 /// Event-based simulators implement this iterator-like trait.
 ///
 /// Each step in the simulation advances an arbitrary amount of time.
-pub trait EventLike {
+pub trait EventLike: Sized {
     /// Advance the simulation to the next enqueued event
     fn next_event(&mut self) -> SimStatus;
+}
+
+/// Iterator wrapper
+pub struct EventIterator<T: EventLike> {
+    inner: T,
+}
+
+// Blanket trait that includes Iterator functionality
+pub trait ToEventIterator: EventLike {
+    fn to_event_iter(self) -> EventIterator<Self>
+    where
+        Self: Sized,
+    {
+        EventIterator { inner: self }
+    }
+}
+
+// blanket impl
+impl<T: EventLike> ToEventIterator for T {}
+
+impl<T: EventLike> Iterator for EventIterator<T> {
+    type Item = SimStatus;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next_event() {
+            SimStatus::Continue => Some(SimStatus::Continue),
+            SimStatus::Stop => None,
+        }
+    }
+}
+
+fn asd<T: Iterator<Item = SimStatus>>(i: T) {
+    for status in i.into_iter() {
+        ()
+    }
+}
+
+struct MySim;
+impl EventLike for MySim {
+    fn next_event(&mut self) -> SimStatus {
+        SimStatus::Continue
+    }
+}
+
+fn def() {
+    let x = MySim;
+
+    let x_i = x.to_event_iter();
+
+    // asd(x_i);
+
+    let executor: SimExecutor<Event, EventIterator<MySim>> = SimExecutor {
+        iter: x_i,
+        _marker: std::marker::PhantomData,
+    };
 }
 
 /// Process-based simulators implement this trait.
 ///
 /// Process-based simulators run with an internal clock, in a tick-by-tick fashion.
-pub trait ProcessLike {
+pub trait ProcessLike: Sized {
     /// Data to be injected into the simulation, if applicable
     type Update;
 
     fn next_tick(&mut self, update: Self::Update) -> SimStatus;
+}
+
+pub struct SimExecutor<T, P: Iterator<Item = SimStatus>> {
+    iter: P,
+
+    _marker: core::marker::PhantomData<T>,
+}
+
+impl<Sim> SimExecutor<Event, Sim>
+where
+    Sim: Iterator<Item = SimStatus>,
+{
+    pub fn from_event(event: Sim) -> Self {
+        Self {
+            iter: event.into_iter(),
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<Sim: Iterator<Item = SimStatus>> SimExecutor<Process, Sim> {
+    pub fn from_process(proc: Sim) -> Self {
+        Self {
+            iter: proc.into_iter(),
+            _marker: std::marker::PhantomData,
+        }
+    }
 }
