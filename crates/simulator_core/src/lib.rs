f1d7845db4a2ae2397069fe266905667baa5681c
Jia Rui <61926974+cruzerngz@users.noreply.github.com>
2024-04-10 22:32:59 +0800

    simplify sim core

--- crates/simulator_core/src/lib.rs
+++ crates/simulator_core/src/lib.rs
@@ -1,18 +1,65 @@
-mod traits;
+//! Core functionality for running simulations.
 
-pub use traits::*;
+use std::io;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait EventLike {
+    type SharedResources: Default;
+    type EventStats: Default;
+
+    /// Step through one event in the simulation.
+    fn step(&mut self, shared: &mut Self::SharedResources) -> Option<Vec<Self::EventStats>>;
+}
+
+/// Runner for event-based simulations
+pub struct EventRunner<P>
+where
+    P: EventLike,
+{
+    inner: P,
+
+    /// Commonly shared resources used in the simulation
+    globals: P::SharedResources,
+
+    /// The results of the simulation
+    results: Vec<P::EventStats>,
+}
+
+impl<P> EventRunner<P>
+where
+    P: EventLike,
+{
+    /// Initialize runner with the event processor.
+    pub fn init(logic: P, resources: Option<P::SharedResources>) -> Self {
+        Self {
+            inner: logic,
+            globals: resources.unwrap_or_default(),
+            results: Vec::new(),
+        }
+    }
+
+    pub fn run(&mut self) {
+        while let Some(stats) = self.inner.step(&mut self.globals) {
+            self.results.extend(stats);
+        }
+    }
+
+    /// Write the results of the simulation as csv to a file.
+    pub fn write_to_file(&self, path: &str) -> io::Result<()>
+    where
+        P::EventStats: serde::Serialize,
+    {
+        let mut writer = csv::Writer::from_path(path)?;
+
+        for record in &self.results {
+            writer.serialize(record)?;
+        }
+
+        writer.flush()
+    }
 }
 
 #[cfg(test)]
+#[allow(unused)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
